66f2c5ad2fa642a684771ba50c1305a394d69eca
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-06-09 12:56:13 +0100

    Tutorial 1 - Learning the basics

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,10 @@
+//Learning the basics
+let js = "amazing";
+console.log(40 + 40);
+console.log("bilal");
+
+let firstName = "BILAL";
+console.log(firstName);
+
+let myFirstJob = "student";
+console.log(myFirstJob);
