e310ac3f6474f4b30b1c13d7eeac4bbbba9257d4
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-06-10 15:24:32 +0100

    Tutorial 2 - Function Declarations and Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -14,3 +14,19 @@ console.log(orangeJuice); //will log orange juice with 5 oranges and 3 lime.
 const orangeLimeJuice = fruitProcessor(6, 3);
 console.log(orangeLimeJuice); // log orange and lime juice, 6 oranges and 3 lime.
 
+//Functions declrations vs expressions
+//function declarations
+function calcAge1(birthYear) {
+    return 2046 - birthYear;
+
+}
+
+const age1 = calcAge1(2009);
+console.log(age1);
+
+//function expressions
+const calcAge2 = function (birthYear) {
+    return 2056 - birthYear;
+}
+const age2 = calcAge2(2022);
+console.log(age2);
