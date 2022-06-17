a9111578620a7bb3793e1d4a1651662f8500d78a
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-06-17 16:26:57 +0100

    JS in Browser: Getting Started and Selecting and Manipulating Elements

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -249,3 +249,29 @@ while (dice !== 6) { // loop will run if "dice" is not 6
         console.log("loop is about to end");
     };
 }
+
+//Coding Challenge 4
+const calcTip = function (bill) {
+    return bill >= 60 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+};
+console.log(bills, tips, totals);
+
+const calcAvg = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(calcAvg([2, 3, 11]));
+console.log(calcAvg(totals));
+console.log(calcAvg(tips));
---
+++ JavaScript in Browser DOM and Event Fundamentals/Guess My Number Project 1/script.js
@@ -0,0 +1,8 @@
+'use strict';
+document.querySelector(".message").textContent = "correct"; //will select message from html and will change the text content.
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20
+document.querySelector('.guess').value = 23;
+
+//handling click events
