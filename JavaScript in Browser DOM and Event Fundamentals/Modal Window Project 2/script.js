9bb8fa75fd5599b7faf96ccc877e7ada8aa05a7f
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-06-19 00:01:50 +0100

    Modal Project 2 - Getting Started Basics

--- JavaScript in Browser DOM and Event Fundamentals/Modal Window Project 2/script.js
+++ JavaScript in Browser DOM and Event Fundamentals/Modal Window Project 2/script.js
@@ -1,1 +1,11 @@
 'use strict';
+//MODAL PROJECT 2
+//Start by selcecting all of the elements
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i <  btnShowModal.length; i++){
+    console.log(btnShowModal[i].textContent);
+};
