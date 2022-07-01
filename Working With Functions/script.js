4c125236ce3b0fc96b910d0ebe86bf9882544439
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-07-01 16:52:31 +0100

    Tutorial 1 - Default Parameters

--- Working With Functions/script.js
+++ Working With Functions/script.js
@@ -1,1 +1,11 @@
 'use strict';
+//defalut parameters
+const bookings = [];
+const createBooking = function (flightNumb, numbPassengers = 1, price = 300) {
+  //we are bale to set a default by using =
+  const booking = { flightNumb, numbPassengers, price };
+  console.log(booking);
+  bookings.push(booking);
+};
+console.log(createBooking('F927')); // this will return "F9227 , 1 ,300"
+console.log(createBooking('F3712', 3, 440)); // this will return "F3712 , 3 ,440"
