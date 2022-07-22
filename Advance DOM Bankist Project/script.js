218ef8c788412bce456f1c7a90ed25373d760067
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-07-22 18:28:24 +0100

    //Passing Arguments to Event Handler (nav bar menu fade)

--- Advance DOM Bankist Project/script.js
+++ Advance DOM Bankist Project/script.js
@@ -67,3 +67,24 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+//Passing Arguments to Event Handler (nav bar menu fade)
+const nav = document.querySelector('.mav');
+
+//menu fade
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover(1));
