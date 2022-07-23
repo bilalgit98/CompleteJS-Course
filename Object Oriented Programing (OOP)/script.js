8aa3c856dd132fcf1e30c57f7a9afad0eb458940
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-07-23 16:01:34 +0100

    Constructor Functions and the new Operator

--- Object Oriented Programing (OOP)/script.js
+++ Object Oriented Programing (OOP)/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const dave = new Person('Dave', 2001);
+
+console.log(dave);
