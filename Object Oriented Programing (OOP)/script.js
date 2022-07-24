af26c228fef1dc878feb682047f891b33fd137e1
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-07-24 14:45:17 +0100

    coding challenge 1

--- Object Oriented Programing (OOP)/script.js
+++ Object Oriented Programing (OOP)/script.js
@@ -7,3 +7,33 @@ const Person = function (firstName, birthYear) {
 const dave = new Person('Dave', 2001);
 
 console.log(dave);
+
+//Prototype
+Person.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+
+dave.calcAge();
+
+//coding challenge 1
+
+const car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+car.prototype.accelarate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+const bmw = new car('Bmw', 120);
+const mercedes = new car('Mercedes', 95);
+
+bmw.accelarate();
+bmw.brake();
