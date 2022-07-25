d9a7752668e40fe22d786db5e9eacc387f0091c6
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-07-25 14:52:44 +0100

    Coding challenge 3

--- Object Oriented Programing (OOP)/script.js
+++ Object Oriented Programing (OOP)/script.js
@@ -59,66 +59,109 @@ account.latest = 50;
 console.log(account.movements);
 
 //coding challenge 2
-class CarCalc {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
+// class CarCalc {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
 
-  accelarate = function () {
-    this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed}`);
-  };
+//   accelarate = function () {
+//     this.speed += 10;
+//     console.log(`${this.make} is going at ${this.speed}`);
+//   };
 
-  brake = function () {
-    this.speed -= 5;
-    console.log(`${this.make} is going at ${this.speed}`);
-  };
+//   brake = function () {
+//     this.speed -= 5;
+//     console.log(`${this.make} is going at ${this.speed}`);
+//   };
 
-  get speedUS() {
-    return this.speed / 1.6;
-  }
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
 
-  set speedUS(speed) {
-    this.speed = speed * 1.6;
-  }
-}
-const ford = new CarCalc('FORD', 120);
-console.log(ford.speedUS);
-ford.speedUS = 60;
-console.log(ford);
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+// const ford = new CarCalc('FORD', 120);
+// console.log(ford.speedUS);
+// ford.speedUS = 60;
+// console.log(ford);
 
-//inheritence
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// //inheritence
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// const dave = new Person('Dave', 2001);
+
+// console.log(dave);
+
+// //Prototype
+// Person.prototype.calcAge = function () {
+//   console.log(2022 - this.birthYear);
+// };
+
+// dave.calcAge();
+
+// const Student = function (firstName, birthYear, course) {
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+
+// Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype.introduce = function () {
+//   console.log(
+//     `My name is ${this.firstName}, and i am studying ${this.course}!`
+//   );
+// };
+
+// const john = new Student('john', 2000, 'Comp Sci');
+// console.log(john);
+// john.introduce();
+// john.calcAge();
+
+//coding challenge 3
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const dave = new Person('Dave', 2001);
-
-console.log(dave);
-
-//Prototype
-Person.prototype.calcAge = function () {
-  console.log(2022 - this.birthYear);
+Car.prototype.accelarate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
 };
 
-dave.calcAge();
-
-const Student = function (firstName, birthYear, course) {
-  Person.call(this, firstName, birthYear);
-  this.course = course;
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}`);
 };
 
-Student.prototype = Object.create(Person.prototype);
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+// creating the link between the prototypes
+EV.prototype = Object.create(Car.prototype);
 
-Student.prototype.introduce = function () {
+//Battery charge
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+//Accelerate
+EV.prototype.accelarate = function () {
+  this.speed += 20;
+  this.charge--;
   console.log(
-    `My name is ${this.firstName}, and i am studying ${this.course}!`
+    `${this.make} is going at ${this.speed} and the battery charge is at ${this.charge}`
   );
 };
 
-const john = new Student('john', 2000, 'Comp Sci');
-console.log(john);
-john.introduce();
-john.calcAge();
+//Creating the EV
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(98); //battery charge should be set to 98
+console.log(tesla);
+tesla.brake();
+tesla.accelarate(); //battery percentage/charge drops by 1
