ccedfb3c7537c42da2dd438442a8849890bc68f2
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-07-27 23:54:35 +0100

    Chaining and Consuming Promises

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -36,23 +36,23 @@ const countriesContainer = document.querySelector('.countries');
 
 // getCountryData('spain');
 
-// const renderCountry = function (data, className) {
-//   const html = `
-//     <article class="country ${className}">
-//         <img class="country__img" src="${data.flag}" />
-//        <div class="country__data">
-//          <h3 class="country__name">${data.name}</h3>
-//         <h4 class="country__region">${data.region}</h4>
-//         <p class="country__row"><span>👫</span>${(
-//           +data.population / 1000000
-//         ).toFixed(1)} People</p>
-//         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//         <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//       </div>
-//     </article>`;
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
+const renderCountry = function (data, className) {
+  const html = `
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+       <div class="country__data">
+         <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} People</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 // const getCountryAndNeighbour = function (country) {
 //   //AJAX call for country no:1
@@ -90,6 +90,19 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryAndNeighbour('gb');
 
 //promises and the fetch API
-
 const request = fetch('https://restcountries.com/v2/name/portugal');
 console.log(request);
+//consuming promises
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+
+getCountryData('portugal');
