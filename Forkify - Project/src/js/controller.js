7dbe00fa7adb478f477383469f397b973a4fe805
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-08-06 16:53:04 +0100

    gitignore added and Loading Recipe from API

--- Forkify - Project/src/js/controller.js
+++ Forkify - Project/src/js/controller.js
@@ -11,3 +11,39 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// loading Recipe from API
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await response.json();
+
+    if (!response.ok) throw new Error(`${data.message} , (${response.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(response, data);
+  } catch (errors) {
+    alert(errors);
+    console.error(errors);
+  }
+};
+
+fetch(
+  'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+);
+
+showRecipe();
